6131902e2627fdf27fd9d0f3a3296cf179b0b8a1
ANDREY STEVEN <andreyleon53@gmail.com>
2025-08-04 23:58:12 -0500

    prueba3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/frontend_avocado/', // 👈 esto debe coincidir con tu repositorio
   plugins: [react()],
+  base: '/frontend_avocado/',
+  build: {
+    outDir: 'dist',
+  },
 })
