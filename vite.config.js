f2470a7198204785583158055253b34bae158c7e
ANDREY STEVEN <andreyleon53@gmail.com>
2025-08-04 23:53:42 -0500

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/frontend_avocado/', // 👈 esto debe coincidir con tu repositorio
+  plugins: [react()],
 })
